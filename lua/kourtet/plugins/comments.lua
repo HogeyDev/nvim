128cd58fbec2a93acf34d0fdb95a91fccb08d355
Evan Hoge <evanhoge123@gmail.com>
2024-07-02 21:46:42 -0400

    added folke/todo-comments for highlighting todo comments and other related ones

--- lua/kourtet/plugins/comments.lua
+++ lua/kourtet/plugins/comments.lua
@@ -1,20 +1,27 @@
 return {
-    "numToStr/Comment.nvim",
-    opts = {},
-    config = function()
-        require("Comment").setup({
-            padding = true,
-            sticky = true,
-            ignore = nil,
-            toggler = {
-                line = '<leader>/',
-            },
-            mappings = {
-                basic = true,
-            },
-            pre_hook = nil,
-            post_hook = nil,
-        }
-        )
-    end
+    {
+        "numToStr/Comment.nvim",
+        opts = {},
+        config = function()
+            require("Comment").setup({
+                padding = true,
+                sticky = true,
+                ignore = nil,
+                toggler = {
+                    line = '<leader>/',
+                },
+                mappings = {
+                    basic = true,
+                },
+                pre_hook = nil,
+                post_hook = nil,
+            }
+            )
+        end
+    },
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {},
+    },
 }
