7b45e13b645ca74054956c1859468ffb24521ad8
Evan Hoge <evanhoge123@gmail.com>
2024-07-15 17:19:29 -0400

    Added mappings <C-j> and <C-k> to move telescope selection

--- lua/kourtet/plugins/telescope.lua
+++ lua/kourtet/plugins/telescope.lua
@@ -7,5 +7,18 @@ return {
     config = function()
         vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
         vim.keymap.set("n", "<leader>fw", require("telescope.builtin").live_grep)
+        vim.keymap.set("n", "<leader>fd", require("telescope.builtin").diagnostics)
+
+        local actions = require("telescope.actions")
+        require("telescope").setup({
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<C-j>"] = actions.move_selection_next,
+                        ["<C-k>"] = actions.move_selection_previous,
+                    }
+                }
+            }
+        })
     end,
 }
