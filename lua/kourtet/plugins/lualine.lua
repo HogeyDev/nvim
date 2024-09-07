734f3f3943fe83ab6d98faa3020f2dbccbffa578
Evan Hoge <evanhoge123@gmail.com>
2024-09-07 14:05:54 -0400

    you know me

--- lua/kourtet/plugins/lualine.lua
+++ lua/kourtet/plugins/lualine.lua
@@ -4,19 +4,20 @@ return {
     config = function()
         local lualine = require('lualine')
 
-        local colors = {
-            bg       = '#202328',
-            fg       = '#bbc2cf',
-            yellow   = '#ecbe7b',
-            cyan     = '#008080',
-            darkblue = '#081633',
-            green    = '#98be65',
-            orange   = '#ff8800',
-            violet   = '#a9a1e1',
-            magenta  = '#c678dd',
-            blue     = '#51afef',
-            red      = '#ec5f67',
-        }
+        local colors = require('tokyonight.colors').setup()
+        -- local colors = {
+        --     bg       = '#202328',
+        --     fg       = '#bbc2cf',
+        --     yellow   = '#ecbe7b',
+        --     cyan     = '#008080',
+        --     darkblue = '#081633',
+        --     green    = '#98be65',
+        --     orange   = '#ff8800',
+        --     violet   = '#a9a1e1',
+        --     magenta  = '#c678dd',
+        --     blue     = '#51afef',
+        --     red      = '#ec5f67',
+        -- }
 
         local conditions = {
             buffer_not_empty = function()
