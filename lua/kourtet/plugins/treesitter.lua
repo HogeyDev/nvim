505f84b1d027b1341e0465594589595098550ddb
Evan Hoge <evanhoge123@gmail.com>
2026-05-07 17:56:25 -0400

    fixed treesitter lol

--- lua/kourtet/plugins/treesitter.lua
+++ lua/kourtet/plugins/treesitter.lua
@@ -3,13 +3,16 @@ return {
     build = ":TSUpdate",
     -- branch = "main",
     -- main = "nvim-treesitter.configs",
-    -- opts = {
-    --     ensure_installed = { "c", "lua", "vim", "go" },
-    --     sync_install = false,
-    --     auto_install = true,
-    --     highlight = {
-    --         enable = true,
-    --         additional_vim_regex_highlighting = false,
-    --     },
-    -- },
+    opts = {
+        ensure_installed = { "c", "lua", "vim", "go" },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
+    },
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+    end,
 }
